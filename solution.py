bc30d45f9a7d0fd91b785a71ce6ba83488264c56
wh2289 <93686988+wh2289@users.noreply.github.com>
2022-02-04 20:28:05 -0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 
 def welcome_assignment_answers(question):
     if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-        answer == "mtls"
+        answer = "mtls"
     elif question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
